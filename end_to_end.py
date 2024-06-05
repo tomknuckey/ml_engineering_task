884a6c28784c9cdbf9806b539857f2bf3dc5e7fc
Tom Knuckey <tomknuckey@hotmail.com>
2024-06-05 20:52:42 +0100

    Created a .py file with seperate utils where mlflow is implemented

--- end_to_end.py
+++ end_to_end.py
@@ -1,105 +1,4 @@
-
-# Data generation function - this is not part of the task, and is only to simulate pulling from a SQL database
-# Don't worry about changing this - it's just to make the notebook a little easier!
 import pandas as pd
-import numpy as np
-from sklearn.datasets import make_classification
-from sklearn.preprocessing import MinMaxScaler
-
-def collect_from_database(query: str) -> pd.DataFrame:
-    print(f"Executing: {query}")
-    n_rows = 10_000
-    n_features = 16
-    features, labels = make_classification(
-        n_samples = n_rows,
-        n_features = 16,
-        n_informative = 7,
-        n_redundant = 4,
-        n_repeated = 3,
-        n_classes = 2,
-        class_sep = 1.2,
-        flip_y = 0.035, # Randomly invert y for added noise
-        weights = [0.85,0.15],
-        random_state = 1889,
-    )
-    df = pd.DataFrame(features, columns=[f'numeric_{i+1}' for i in range(n_features)])
-    df.insert(value=labels, loc=0, column='claim_status')
-    df = df.rename(columns={
-        'numeric_1': 'age',
-        'numeric_2': 'height_cm',
-        'numeric_3': 'weight_kg',
-        'numeric_4': 'income',
-        'numeric_5': 'financial_hist_1',
-        'numeric_6': 'financial_hist_2',
-        'numeric_7': 'financial_hist_3',
-        'numeric_8': 'financial_hist_4',
-        'numeric_9': 'credit_score_1',
-        'numeric_10': 'credit_score_2',
-        'numeric_11': 'credit_score_3',
-        'numeric_12': 'insurance_hist_1',
-        'numeric_13': 'insurance_hist_2',
-        'numeric_14': 'insurance_hist_3',
-        'numeric_15': 'insurance_hist_4',
-        'numeric_16': 'insurance_hist_5',
-    })
-    df['age'] = MinMaxScaler(feature_range=(18, 95)).fit_transform(df['age'].values[:, None])
-    df['age'] = df['age'].astype('int')
-    df['height_cm'] = MinMaxScaler(feature_range=(140, 210)).fit_transform(df['height_cm'].values[:, None])
-    df['height_cm'] = df['height_cm'].astype('int')
-    df['weight_kg'] = MinMaxScaler(feature_range=(45, 125)).fit_transform(df['weight_kg'].values[:, None])
-    df['weight_kg'] = df['weight_kg'].astype('int')
-    df['income'] = MinMaxScaler(feature_range=(0, 250_000)).fit_transform(df['income'].values[:, None])
-    df['income'] = df['income'].astype('int')
-    df['credit_score_1'] = MinMaxScaler(feature_range=(0, 999)).fit_transform(df['credit_score_1'].values[:, None])
-    df['credit_score_1'] = df['credit_score_1'].astype('int')
-    df['credit_score_2'] = MinMaxScaler(feature_range=(0, 700)).fit_transform(df['credit_score_2'].values[:, None])
-    df['credit_score_2'] = df['credit_score_2'].astype('int')
-    df['credit_score_3'] = MinMaxScaler(feature_range=(0, 710)).fit_transform(df['credit_score_3'].values[:, None])
-    df['credit_score_3'] = df['credit_score_3'].astype('int')
-    df['bmi'] = (df['weight_kg']/((df['height_cm']/100)**2)).astype('int')
-    df['gender'] = np.where(
-        df['claim_status'] == 0,
-        np.random.choice([1, 0], size=(n_rows), p=[0.46, 0.54]),
-        np.random.choice([1, 0], size=(n_rows), p=[0.52, 0.48])
-        )
-    df['marital_status'] = np.random.choice(['A', 'B', 'C', 'D', 'E', 'F'], size=(n_rows), p=[0.2, 0.15, 0.1, 0.25, 0.15, 0.15])
-    df['occupation'] = np.random.choice(['A', 'B', 'C', 'D', 'E', 'F', 'G'], size=(n_rows))
-    df['location'] = np.random.choice(list(string.ascii_uppercase), size=(n_rows))
-    df['prev_claim_rejected'] = np.where(
-        df['claim_status'] == 0,
-        np.random.choice([1, 0], size=(n_rows), p=[0.08, 0.92]),
-        np.random.choice([1, 0], size=(n_rows), p=[0.16, 0.84])
-        )
-    df['known_health_conditions'] = np.random.choice([1, 0], size=(n_rows), p=[0.06, 0.94])
-    df['uk_residence'] = np.random.choice([1, 0], size=(n_rows), p=[0.76, 0.24])
-    df['family_history_1'] = np.random.choice([1, 0], size=(n_rows), p=[0.22, 0.78])
-    df['family_history_2'] = np.random.choice([1, 0], size=(n_rows), p=[0.25, 0.75])
-    df['family_history_3'] = np.random.choice([1, None, 0], size=(n_rows), p=[0.12, 0.81, 0.07])
-    df['family_history_4'] = np.random.choice([1, 0], size=(n_rows), p=[0.27, 0.73])
-    df['family_history_5'] = np.random.choice([1, 0], size=(n_rows), p=[0.31, 0.69])
-    df['product_var_1'] = np.random.choice([1, 0], size=(n_rows), p=[0.38, 0.62])
-    df['product_var_2'] = np.random.choice([1, 0], size=(n_rows), p=[0.55, 0.45])
-    df['product_var_3'] = np.random.choice(['A', 'B', 'C', 'D'], size=(n_rows), p=[0.23, 0.28, 0.31, 0.18])
-    df['product_var_4'] = np.random.choice([1, 0], size=(n_rows), p=[0.76, 0.24])
-    df['health_status'] = np.random.randint(1, 5, size=(n_rows))
-    df['driving_record'] = np.random.randint(1, 5, size=(n_rows))
-    df['previous_claim_rate'] = np.where(
-        df['claim_status'] == 0,
-        np.random.choice([1, 2, 3, 4, 5], size=(n_rows), p=[0.48, 0.29, 0.12, 0.08, 0.03]),
-        np.random.choice([1, 2, 3, 4, 5], size=(n_rows), p=[0.12, 0.28, 0.34, 0.19, 0.07]),
-    )
-    df['education_level'] = np.random.randint(0, 7, size=(n_rows))
-    df['income level'] = pd.cut(df['income'], bins=5, labels=False, include_lowest=True)
-    df['n_dependents'] = np.random.choice(
-        [1, 2, 3, 4, 5], size=(n_rows), p=[0.23, 0.32, 0.27, 0.11, 0.07]
-    )
-    df['employment_type'] = np.random.choice(
-        [1, None, 0], size=(n_rows), p=[0.16, 0.7, 0.14]
-    )
-    return df
-
-
-import pandas as pan
 import numpy as np
 from scipy import stats
 from sklearn.datasets import *
@@ -111,6 +10,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import string
 import shap
+import mlflow
+from mlflow.models import infer_signature
+import os
+
+from end_to_end_utils import collect_from_database
 
 #Seed
 np.random.seed(1889)
@@ -118,7 +22,7 @@ np.random.seed(1889)
 
 account_name = "rg_data_sci"
 client_id = "a1b2c3d4"
-client_secret = "b1c2d3e4" #Delete before commiting to ADO!!!
+#client_secret = "b1c2d3e4" #Delete before commiting to ADO!!!
 subscription_id = "8a7b6c5d"
 tenant_id = "6f5g4h3i"
 datalake_name = "rg_data_lake"
@@ -126,7 +30,6 @@ datalake_name = "rg_data_lake"
 dataset_from_database = collect_from_database(
     "SELECT * FROM CLAIMS.DS_DATASET"
 )
-dataset_from_database.head(10)
 
 total = dataset_from_database.isnull().sum()
 percent = (dataset_from_database.isnull().sum() / dataset_from_database.isnull().count()*100)
@@ -283,8 +186,12 @@ f"The Cohen Kappa score on the test data is: {kappa2}"
 )
 
 print()
-print("The accuracy on train dataset is: ", accuracy_score(y_train, train_class_preds2))
-print("The accuracy on test dataset is: ", accuracy_score(y_test, test_class_preds2))
+
+training_accuracy = accuracy_score(y_train, train_class_preds2)
+print("The accuracy on train dataset is: ", training_accuracy)
+
+test_accuracy = accuracy_score(y_test, test_class_preds2)
+print("The accuracy on test dataset is: ", test_accuracy)
 
 print()
 print("Train confusion matrix: ", confusion_matrix(y_train, train_class_preds2))
@@ -312,3 +219,33 @@ print()
 print("F1 score is: ", f1_score(y_test, test_class_preds2))
 print("Precision is: ", precision_score(y_test, test_class_preds2))
 print("Recall is: ", recall_score(y_test, test_class_preds2))
+
+# Set our tracking server uri for logging
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+
+# Create a new MLflow Experiment
+mlflow.set_experiment("MLflow Quickstart")
+
+# Start an MLflow run
+with mlflow.start_run():
+    # Log the hyperparameters
+    mlflow.log_params(parameter_gridSearch.best_params_)
+
+    # Log the loss metric
+    mlflow.log_metric("test_accuracy", test_accuracy)
+    mlflow.log_metric("training_accuracy", training_accuracy)
+
+    # Set a tag that we can use to remind ourselves what this run was for
+    mlflow.set_tag("Training Info", "Model for credit model")
+
+    # Infer the model signature
+    signature = infer_signature(X_train, parameter_gridSearch.predict(X_train))
+
+    # Log the model
+    model_info = mlflow.sklearn.log_model(
+        sk_model=parameter_gridSearch,
+        artifact_path="credit_model",
+        signature=signature,
+        input_example=X_train,
+        registered_model_name="tracking-quickstart",
+    )
--- readme.md
+++ readme.md
@@ -1,1 +1,16 @@
 # How to Run
+
+
+
+## MLFlow
+
+I have created MLFlow to experiment changing the model to track how it's changed
+
+This will allow me to promote the best model to production
+
+### Original Trial
+
+The accuracy on train dataset is:  0.9815
+The accuracy on test dataset is:  0.959
+
+This is overfitting
